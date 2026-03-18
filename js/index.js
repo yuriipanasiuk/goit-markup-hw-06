ae810f69f8ebea2b4de7c4d2d9699767a737aa22
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2026-03-18 13:42:54 +0200

    updated styles for menu

--- js/index.js
+++ js/index.js
@@ -3,12 +3,23 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    body: document.body,
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-open");
+  }
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-open");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
